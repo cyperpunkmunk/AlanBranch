6c35ad06c5ac138aa1cf1a214daa9350b7174a78
Alien <kendgibsonjr@gmail.com>
2021-05-13 15:46:51 -0500

    added mobile nav

--- js/app.js
+++ js/app.js
@@ -9,3 +9,13 @@ $(document).scroll(function() {
   });
 
 
+function openNav() {
+
+    document.getElementById("open").style.display = "none";
+    document.getElementById("nav").style.width = "100%"
+}
+
+function closeNav() {
+    document.getElementById("nav").style.width = "0";
+    document.getElementById("open").style.display  = "block"
+}
